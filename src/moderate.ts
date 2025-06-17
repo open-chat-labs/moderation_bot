57241ed233cfa223e1ebfb7e10156fcce0e04f99
Julian Jelfs <julian.jelfs@gmail.com>
2025-06-17 17:55:39 +0100

    make sure we can also moderation images

--- src/moderate.ts
+++ src/moderate.ts
@@ -3,6 +3,7 @@ import {
   BotClient,
   ChatActionScope,
   CommunityIdentifier,
+  ImageContent,
   MessageContent,
 } from "@open-ic/openchat-botclient-ts";
 import OpenAI from "openai";
@@ -110,32 +111,38 @@ async function askOpenAI(
   return { kind: "not_moderated" };
 }
 
+function buildImageUrl(content: ImageContent): string | undefined {
+  if (content.blobReference) {
+    return `https://${content.blobReference?.canisterId}.raw.icp0.io/blobs/${content.blobReference?.blobId}`;
+  }
+}
+
 // It is quite common for chat rules to forbid certain types of message in the main chat but allow
 // it in a thread
 function extractFromContent(
   content: MessageContent
-): [string | undefined, string] | undefined {
+): [string | undefined, string, string | undefined] | undefined {
   switch (content.kind) {
     case "audio_content":
-      return [content.caption, "Audio message"];
+      return [content.caption, "Audio message", undefined];
     case "video_content":
-      return [content.caption, "Video message"];
+      return [content.caption, "Video message", undefined];
     case "crypto_content":
-      return [content.caption, "Crypto transfer"];
+      return [content.caption, "Crypto transfer", undefined];
     case "file_content":
-      return [content.caption, "File message"];
+      return [content.caption, "File message", undefined];
     case "giphy_content":
-      return [content.caption, "Gif message"];
+      return [content.caption, "Gif message", undefined];
     case "image_content":
-      return [content.caption, "Image message"];
+      return [content.caption, "Image message", buildImageUrl(content)];
     case "p2p_swap_content":
-      return [content.caption, "Swap message"];
+      return [content.caption, "Swap message", undefined];
     case "poll_content":
-      return [content.config.text, "Poll message"];
+      return [content.config.text, "Poll message", undefined];
     case "prize_content":
-      return [content.caption, "Prize message"];
+      return [content.caption, "Prize message", undefined];
     case "text_content":
-      return [content.text, "Text message"];
+      return [content.text, "Text message", undefined];
     default:
       return undefined;
   }
@@ -171,7 +178,7 @@ export async function moderateMessage(
     const eventIndex = resp.events[0].index;
     const content = extractFromContent(resp.events[0].event.content);
     if (content !== undefined) {
-      const [txt, hint] = content;
+      const [txt, hint, imageUrl] = content;
 
       let result: Moderation = { kind: "not_moderated" };
       if (policy.rules !== Rules.CHAT_RULES && txt !== undefined) {
@@ -179,6 +186,7 @@ export async function moderateMessage(
           client,
           policy,
           txt,
+          imageUrl,
           messageId,
           eventIndex,
           messageIndex
@@ -198,7 +206,6 @@ export async function moderateMessage(
         }
       }
 
-      console.log("Moderation result: ", result);
       if (result.kind === "moderated") {
         await messageBreaksTheRules(client, policy, result, thread);
       }
@@ -248,17 +255,24 @@ export async function generalModeration(
   client: BotClient,
   policy: Policy,
   text: string,
+  imageUrl: string | undefined,
   messageId: bigint,
   eventIndex: number,
   messageIndex: number
 ): Promise<Moderation> {
   console.log("Message text: ", text);
 
+  const inputs: OpenAI.ModerationMultiModalInput[] = [{ type: "text", text }];
+  if (imageUrl !== undefined) {
+    inputs.push({ type: "image_url", image_url: { url: imageUrl } });
+  }
+
   const moderation = await openai.moderations.create({
     model: "omni-moderation-latest",
-    input: text,
+    input: inputs,
   });
   if (moderation.results.length > 0) {
+    console.log("Moderation result: ", moderation.results);
     const result = moderation.results[0];
     const breaking = categoriesThatBreakThreshold(
       policy.threshold,
